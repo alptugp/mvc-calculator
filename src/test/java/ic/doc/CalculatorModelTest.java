a2ae717b919a9d4c0cb83088b18833c17869bdd1
alptugp <alptugati@gmail.com>
2022-11-17 18:58:47 +0000

    Refactor Google style checks 2

--- src/test/java/ic/doc/CalculatorModelTest.java
+++ src/test/java/ic/doc/CalculatorModelTest.java
@@ -6,7 +6,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class CalculatorModelTest {
   private CalculatorModel calculatorModel;
